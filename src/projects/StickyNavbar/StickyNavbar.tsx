760dcd2a1e60f0cc3974866af4535818de608fc1
ozanbatuhankurucu <ozanbatusss@gmail.com>
2025-03-23 23:04:12 +0300

    Add content section

--- src/projects/StickyNavbar/StickyNavbar.tsx
+++ src/projects/StickyNavbar/StickyNavbar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Content from './components/Content'
 
 export const StickyNavbar = () => (
   <>
     <Navbar />
     <Hero />
+    <Content />
   </>
 )
