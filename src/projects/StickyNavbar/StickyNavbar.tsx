0778a07487f145a0010d8c4d227d48717cfc3e12
ozanbatuhankurucu <ozanbatusss@gmail.com>
2025-03-30 19:10:14 +0300

    Add ThemeProvider into StickyNavbar component

--- src/projects/StickyNavbar/StickyNavbar.tsx
+++ src/projects/StickyNavbar/StickyNavbar.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Content from './components/Content'
+import { ThemeProvider } from './ThemeContext'
 
 export const StickyNavbar = () => (
-  <>
+  <ThemeProvider>
     <Navbar />
     <Hero />
     <Content />
-  </>
+  </ThemeProvider>
 )
